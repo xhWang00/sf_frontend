6077549333b53ec232882c5d8e75f77e4ca35936
neilsen <daneilsen96@gmail.com>
2023-04-19 21:08:05 -0700

    Added Sensor Detections Stream

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,12 @@ import { useState, useEffect } from 'react';
 import { ReactCompareSlider, ReactCompareSliderImage } from 'react-compare-slider';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { Route, Routes} from "react-router";
-import './FrontPage.css';
+import './styles.css';
 
 
 const BASEURL = 'http://127.0.0.1:5000/frame/2011_09_26_drive_0048_extract.json/0000000000';
 
-function FrontPage() {
-  return (
-    <div className="container">
-      <h1 className="title">Select an Option</h1>
-      <div className="buttons">
-        <Link className="button" to="/upload">
-          Upload a Dataset
-        </Link>
-        <Link className="button" to="/image-comparison">
-          Select a Dataset
-        </Link>
-      </div>
-    </div>
-  );
-}
-
-function Upload() {
-  // Handle file upload logic here
-  return <div>Upload Page</div>;
-}
-
-function SelectDataset() {
-  // Fetch available datasets and handle dataset selection logic here
-  return <div>Select Dataset Page</div>;
-}
-
-function ImageComparison() {
+function App() {
   const [states, setStates] = useState({});
   const [startTime, setStartTime] = useState(null);
 
@@ -57,20 +31,29 @@ function ImageComparison() {
     return (
       <div>
         <h1>2011_09_26_drive_0052_extract</h1>
-        <ReactCompareSlider
-          itemOne={
-            <ReactCompareSliderImage
-            src={`http://127.0.0.1:5000/video_feed/edgless?start=${startTime}`}
-            alt="Image one"
-          />
-          }
-          itemTwo={
-            <ReactCompareSliderImage
-            src={`http://127.0.0.1:5000/video_feed/edges?start=${startTime}`}
-            alt="Image two"
+        <div className="image-container">
+          <div className="react-compare-slider">
+            <ReactCompareSlider
+              itemOne={
+                <ReactCompareSliderImage
+                  src={`http://127.0.0.1:5000/video_feed/edgless`}
+                  alt="Image one"
+                />
+              }
+              itemTwo={
+                <ReactCompareSliderImage
+                  src={`http://127.0.0.1:5000/video_feed/edges`}
+                  alt="Image two"
+                />
+              }
             />
-          }
-        />
+          </div>
+          <img
+            className="third-image"
+            src={`http://127.0.0.1:5000/video_feed/plot`}
+            alt="Image three"
+          />
+        </div>
         <p>
           Computer vision: <b>Not safe</b>
         </p>
@@ -79,21 +62,6 @@ function ImageComparison() {
         </p>
       </div>
     );
-}
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<FrontPage />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/select" element={<SelectDataset />} />
-          <Route path="/image-comparison" element={<ImageComparison />} />
-        </Routes>
-      </div>
-    </Router>
-  );
 }
 
 export default App;
